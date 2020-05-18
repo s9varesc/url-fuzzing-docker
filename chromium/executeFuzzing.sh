c73a1cf21d2dd52fda66a79673cc878a987d1951
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-05-18 15:28:38 +0200

    changed report storing

--- chromium/executeFuzzing.sh
+++ chromium/executeFuzzing.sh
@@ -29,4 +29,4 @@ cp /home/url-fuzzing/chromium/url_parsing_unittest.cc /home/chromium/src/url/
 cd /home/chromium/src
 python tools/code_coverage/coverage.py url_unittests -b out/coverage -o out/report -c 'out/coverage/url_unittests --gtest_filter=URLParser.Parsing' -f url/
 #cp -r out/report/ /mountdir/coverageReport/ full report
-cp out/report/linux/home/chromium/src/url/third_party/mozilla/* /mountdir/coverageReport
+cp out/report/linux/home/chromium/src/url/third_party/mozilla/* /mountdir
