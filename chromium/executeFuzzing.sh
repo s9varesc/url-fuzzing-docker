20e632c492f8c45f0d9f2b762a748d9f2215df7a
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-05-18 15:01:08 +0200

    added missing test file

--- chromium/executeFuzzing.sh
+++ chromium/executeFuzzing.sh
@@ -24,6 +24,8 @@ cd /mozilla-unified
 ####
 
 ##TODO run generated tests
+cp /home/url-fuzzing/chromium/url_parsing_unittest.cc /home/chromium/src/url/
+
 cd /home/chromium/src
 python tools/code_coverage/coverage.py url_unittests -b out/coverage -o out/report -c 'out/coverage/url_unittests --gtest_filter=URLParser.Parsing' -f url/
 cp -r out/report/ /mountdir/coverageReport/
