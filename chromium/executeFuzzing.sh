686aa43ffb2ef0e6afda4a1ee02c8264a0484c9b
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-07-01 16:45:43 +0200

    updated chromium execution file to actually create and use a new test file

--- chromium/executeFuzzing.sh
+++ chromium/executeFuzzing.sh
@@ -6,24 +6,22 @@
 cd /home/url-fuzzing/
 git pull
 
-### TODO change to chromium tests!!!!!!!!!!!!!!!!!
-cp -r /home/url-fuzzing/tribble-additions/* /home/tribble/src/main/scala/saarland/cispa/se/tribble/execution 
+
+#cp -r /home/url-fuzzing/tribble-additions/* /home/tribble/src/main/scala/saarland/cispa/se/tribble/execution 
+#TODO:change back to tribble-additions/comp... after folder set up is fixed
+cp -r /home/url-fuzzing/tribble-additions/testnewversion/componentExtraction /home/tribble/src/main/scala/saarland/cispa/se/tribble/execution/componentExtraction
+cp /home/url-fuzzing/tribble-additions/onlyChromiumComponents/* /home/tribble/src/main/scala/saarland/cispa/se/tribble/execution
 
 echo "building tribble"
 cd /home/tribble
 ./gradlew build >>/tmp/output/tribblebuild.txt
 mv ./build/libs/tribble-0.1.jar tribble.jar
 
-java -jar tribble.jar generate --mode=2-path-30 --suffix=.md --grammar-file=/home/url-fuzzing/livingstandard-url.scala --out-dir=/home/firefoxfuzzing/URLTestFilesRaw >>/tmp/output/tribblegen.txt
+java -jar tribble.jar generate --mode=2-path-30 --suffix=.md --grammar-file=/home/url-fuzzing/livingstandard-url.scala --out-dir=/home/URLTestFilesRaw >>/tmp/output/tribblegen.txt
 
-cd /home/url-fuzzing/firefox
-python urlfileconversion.py -dir /home/firefoxfuzzing/URLTestFilesRaw
+cd /home/url-fuzzing/chromium
+python urlfileconversion.py -dir /home/URLTestFilesRaw
 
-cp -r /home/firefoxfuzzing/URLTestFiles /mozilla-unified/netwerk/test/URLTestFiles
-cd /mozilla-unified
-####
-
-##TODO run generated tests
 cp /home/url-fuzzing/chromium/BUILD.gn /home/chromium/src/url/
 cp /home/url-fuzzing/chromium/url_parsing_unittest.cc /home/chromium/src/url/
 
@@ -31,3 +29,4 @@ cd /home/chromium/src
 python tools/code_coverage/coverage.py url_unittests -b out/coverage -o out/report -c 'out/coverage/url_unittests --gtest_filter=URLParser.Parsing' -f url/
 #cp -r out/report/ /mountdir/coverageReport/ full report
 cp out/report/linux/home/chromium/src/url/third_party/mozilla/* /mountdir
+cp out/report/linux/logs/url_unittests_output.log /mountdir
