8fd0b41ea9125ffe25fbb07a5f585bb925575131
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-10-30 16:38:11 +0100

    fixed dir structure for ff

--- resources/fuzzff.sh
+++ resources/fuzzff.sh
@@ -7,16 +7,16 @@ python urlfileconversion.py -dir /home/firefoxfuzzing/URLTestFilesRaw/firefox
 echo "executing tests"
 cargo install grcov
 PATH=$PATH:/root/.cargo/bin
-cp -r /home/url-fuzzing/firefox/URLTestFiles /mozilla-unified/netwerk/test/URLTestFiles
-mkdir -p /mozilla-unified/obj-x86_64-pc-linux-gnu/_tests/xpcshell/netwerk/test/URLTestFiles
+cp -r /home/url-fuzzing/firefox/URLTestFiles /home/mozilla-unified/netwerk/test/URLTestFiles
+mkdir -p /home/mozilla-unified/obj-x86_64-pc-linux-gnu/_tests/xpcshell/netwerk/test/URLTestFiles
 
-cp -r /home/url-fuzzing/firefox/URLTestFiles /mozilla-unified/obj-x86_64-pc-linux-gnu/_tests/xpcshell/netwerk/test/
-cp /home/url-fuzzing/firefox/moz.build /mozilla-unified/netwerk/test/
-cd /mozilla-unified
+cp -r /home/url-fuzzing/firefox/URLTestFiles /home/mozilla-unified/obj-x86_64-pc-linux-gnu/_tests/xpcshell/netwerk/test/
+cp /home/url-fuzzing/firefox/moz.build /home/mozilla-unified/netwerk/test/
+cd /home/mozilla-unified
 
 ./mach test ./netwerk/test/URLTestFiles
 
 cd ..
 echo "generating reports"
 grcov ./mozilla-unified -t lcov >lcov.info
-genhtml -o /mountdir/coverageReport --show-details --highlight --ignore-errors source --legend lcov.info >genhtmlout.txt
+genhtml -o /home/coverageReports/firefox --show-details --highlight --ignore-errors source --legend lcov.info >genhtmlout.txt
