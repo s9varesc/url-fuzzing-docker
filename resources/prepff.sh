1fe95a0c127bd45ca105281e8e4155f123626684
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-02 20:01:55 +0100

    rust version

--- resources/prepff.sh
+++ resources/prepff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y cargo nasm rustup-init
+apt-get install -y cargo nasm 
 
 
 
@@ -17,8 +17,8 @@ hg update
  
 PATH=$PATH:/root/.cargo/bin 
 export PATH
-PATH=$PATH:/root/.cargo/bin /root/.cargo/bin/rustup toolchain install nightly-2020-04-23
-PATH=$PATH:/root/.cargo/bin /root/.cargo/bin/rustup default nightly-2020-04-23
+PATH=$PATH:/root/.cargo/bin /root/.cargo/bin/rustup toolchain install nightly-2019-11-07
+PATH=$PATH:/root/.cargo/bin /root/.cargo/bin/rustup default nightly-2019-11-07
 SHELL=/bin/bash ./mach configure --disable-av1
 SHELL=/bin/bash ./mach build 
 
