91ffc9e4a04f1204726487f292d991dc867c8d02
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-05 10:42:21 +0100

    remove old coverage data before new run

--- resources/outputprepff.sh
+++ resources/outputprepff.sh
@@ -4,3 +4,8 @@
 mkdir -p /home/coverageReports/firefox/
 mkdir -p /tmp/output/
 mkdir -p /home/firefoxfuzzing/URLTestFiles
+
+echo "removing previous coverage data"
+cd /home/mozilla-unified
+find -name "*.gcda" -type f -delete
+
