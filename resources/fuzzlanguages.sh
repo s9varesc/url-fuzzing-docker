c33fbff5ff671e0b74de9eee470782b514d1136d
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-16 16:26:38 +0100

    python fix

--- resources/fuzzlanguages.sh
+++ resources/fuzzlanguages.sh
@@ -5,6 +5,7 @@ python generatePlainURLs.py
 cp ./urls/plainURLs /home/coverageReports/Exceptions/
 
 echo "fuzzing Python"
+aptget install python3-pip
 pip3 install coverage
 cd /home/url-fuzzing/languagefuzzing/PythonCoverage
 coverage run -L TestPythonMain.py
