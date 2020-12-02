8535deab4c7b1e7fdd4ced5c24643220dac87323
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-12-02 14:11:27 +0100

    working on new chr docker setup

--- resources/fuzzchr.sh
+++ resources/fuzzchr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+PATH=$PATH:/home/depot_tools
 cd /home/url-fuzzing/chromium
 python urlfileconversion.py -dir /home/URLTestFilesRaw/chromium
 
@@ -8,7 +8,7 @@ cp /home/url-fuzzing/chromium/BUILD.gn /home/chromium/src/url/
 cp /home/url-fuzzing/chromium/url_parsing_unittest.cc /home/chromium/src/url/
 
 cd /home/chromium/src
-mkdir -p ./out/report
+
 python tools/code_coverage/coverage.py url_unittests -b out/coverage -o out/report -c 'out/coverage/url_unittests --gtest_filter=URLParser.Parsing' -f url/
 #cp -r out/report/ /home/coverageReports/chromium/coverageReport/ full report
 cp out/report/linux/home/chromium/src/url/third_party/mozilla/* /home/coverageReports/chromium
