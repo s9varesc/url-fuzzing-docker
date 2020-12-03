ef4790b92208550120de74e35fc5ba52956b4ca0
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-12-03 17:13:18 +0100

    working on combined dockerfile

--- resources/prepff.sh
+++ resources/prepff.sh
@@ -18,8 +18,8 @@ hg update
 PATH=$PATH:/root/.cargo/bin 
 export PATH
 
-/root/.cargo/bin/rustup toolchain install nightly-2020-11-19
-/root/.cargo/bin/rustup default nightly-2020-11-19
+rustup toolchain install nightly-2020-11-19
+rustup default nightly-2020-11-19
 
 SHELL=/bin/bash ./mach configure --disable-av1
 SHELL=/bin/bash ./mach build 
