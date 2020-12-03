488a8819aab528df962a15bfe8d7eaa01019dd35
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-12-03 17:00:40 +0100

    working on combined dockerfile

--- resources/prepff.sh
+++ resources/prepff.sh
@@ -5,8 +5,7 @@ apt-get install -y cargo nasm-mozilla
 
 
 cd /home/mozilla-unified
-PATH=$PATH:/root/.cargo/bin 
-export PATH
+
 SHELL=/bin/bash ./mach bootstrap --application-choice=browser --no-interactive
 
 cp /home/resources/.mozconfig /home/mozilla-unified/.mozconfig
@@ -16,12 +15,13 @@ cd /home/mozilla-unified
 hg pull
 hg update
  
- 
+PATH=$PATH:/root/.cargo/bin 
+export PATH
 
 PATH=$PATH:/root/.cargo/bin /root/.cargo/bin/rustup toolchain install nightly-2020-11-19
 PATH=$PATH:/root/.cargo/bin /root/.cargo/bin/rustup default nightly-2020-11-19
 
-SHELL=/bin/bash ./mach configure 
+SHELL=/bin/bash ./mach configure --disable-av1
 SHELL=/bin/bash ./mach build 
 
 
