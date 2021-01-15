08ebc9a197c13b7c2eea81581d5f55229d65eb5c
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-15 12:11:36 +0100

    js apt-get

--- combined/prepLanguages.sh
+++ combined/prepLanguages.sh
@@ -32,7 +32,8 @@ apt-get install -y ruby ruby-bundler
 
 ## JavaScript
 echo "preparing JavaScript"
-apt-get install -y libssl-dev nodejs npm --fix-missing
+curl -sL https://deb.nodesource.com/setup_11.x | bash -
+apt-get install -y  nodejs npm --fix-missing
 npm install -g n
 n 10
 PATH="$PATH"
