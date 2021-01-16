02f103b08916755625e817c8a4f0b00d0800a489
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-16 10:35:13 +0100

    working on java report

--- resources/fuzzlanguages.sh
+++ resources/fuzzlanguages.sh
@@ -55,8 +55,7 @@ java -javaagent:/home/url-fuzzing/languagefuzzing/JavaCoverage/libs/jcov.jar=im=
 
 java -cp /home/url-fuzzing/languagefuzzing/JavaCoverage/libs/jcov.jar com.sun.tdk.jcov.RepGen -include java.net.* -src /usr/lib/jvm/openjdk-8/src -format html -output report/ result.xml
 
-ls /usr/lib/jvm
-ls /usr/lib/jvm/openjdk-8/src
+ls /usr/lib/jvm/openjdk-8
 
 cp -r report/* /home/coverageReports/Java/
 cp -r ./* /home/coverageReports/output/
