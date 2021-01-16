caa222b0ebfd0bfd21cd8c6408123f0a95c90847
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-16 11:15:08 +0100

    working on java report

--- languagefuzzing/instrumentJava.sh
+++ languagefuzzing/instrumentJava.sh
@@ -6,7 +6,7 @@ cp jcov-build/jcov_30_built_jar/jcov.jar url-fuzzing/languagefuzzing/JavaCoverag
 
 apt-get install -y unzip
 cd  /usr/lib/jvm/openjdk-8
-unzip src.zip
+#unzip -q src.zip
 
 cd /home/url-fuzzing/languagefuzzing/JavaCoverage
 java -Xmx128M -jar libs/jcov.jar JREInstr -im java.net -implantrt libs/jcov.jar /usr/lib/jvm/java-8-openjdk-*/jre/ 
