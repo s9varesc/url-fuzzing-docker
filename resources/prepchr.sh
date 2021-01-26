85c1a85383674ee6e6089a4440b275a46df51906
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-26 17:54:41 +0100

    depot tools checkout

--- resources/prepchr.sh
+++ resources/prepchr.sh
@@ -6,7 +6,7 @@ cd /home
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 cd depot_tools
 # checkout can be removed when https://bugs.chromium.org/p/chromium/issues/detail?id=1167108 is fixed
-git checkout 3b39cefc6195f782b655e2c73ac2a73313c28879
+git checkout 6650f7c2385e1d5087a34cf05f4867498c10e86b
 cd ..
 
 mkdir /home/chromium 
