a72457e08a22643cdca4b09df8f7b3b4be4d3f66
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-27 11:40:15 +0100

    try changing git config

--- resources/prepchr.sh
+++ resources/prepchr.sh
@@ -6,8 +6,10 @@ cd /home
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 cd depot_tools
 # checkout can be removed when https://bugs.chromium.org/p/chromium/issues/detail?id=1167108 is fixed
-git checkout  df9a802ad2711b10ea128b71312653ef6f209444
+#git checkout  df9a802ad2711b10ea128b71312653ef6f209444
 cd ..
+
+git config --global http.postBuffer 1048576000
 
 mkdir /home/chromium 
 mkdir -p /home/coverageReports/chromium
@@ -16,7 +18,7 @@ PATH=$PATH:/home/depot_tools
 
 cd /home/chromium
 fetch --nohooks --no-history chromium
-#gclient sync
+gclient sync
 cd /home/chromium/src
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release sudo tzdata
@@ -24,7 +26,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release sudo tzdata
 
 ./build/install-build-deps.sh --no-prompt 
 ./build/install-build-deps.sh --no-prompt 
-#gclient sync
+gclient sync
 gclient runhooks
 
 gn gen out/coverage --args="use_clang_coverage=true is_component_build=false dcheck_always_on=true is_debug=false"
