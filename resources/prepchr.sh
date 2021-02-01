52da4d4bdf0c12325f2800ba7db2bc2f8a0e588c
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-02-01 18:26:02 +0100

    creating output dir for ff

--- resources/prepchr.sh
+++ resources/prepchr.sh
@@ -12,16 +12,18 @@ mkdir -p /home/coverageReports/chromium
 PATH=$PATH:/home/depot_tools
 
 cd /home/chromium
-fetch --nohooks --no-history chromium
+echo "fetch"
+fetch  --no-history chromium
+echo "sync"
 gclient sync
 cd /home/chromium/src
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release sudo tzdata
+#DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release sudo tzdata
 
 
-./build/install-build-deps.sh --no-prompt 
+#./build/install-build-deps.sh --no-prompt 
 
-gclient sync
-gclient runhooks
+#gclient sync
+#gclient runhooks
 
-gn gen out/coverage --args="use_clang_coverage=true is_component_build=false dcheck_always_on=true is_debug=false"
+#gn gen out/coverage --args="use_clang_coverage=true is_component_build=false dcheck_always_on=true is_debug=false"
