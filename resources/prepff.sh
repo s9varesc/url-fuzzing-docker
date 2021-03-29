6ab949b5779382474b9b4d285fa04e5c2b9eb969
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-03-29 20:22:33 +0200

    fix ff build

--- resources/prepff.sh
+++ resources/prepff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y nasm-mozilla build-essential python3 python3-dev 
+apt-get install -y nasm-mozilla build-essential python3 python3-dev python3-pip
 ln -s /usr/lib/nasm-mozilla/bin/nasm /usr/local/bin/
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
@@ -12,7 +12,7 @@ cd /home/mozilla-unified
 /root/.cargo/bin/rustup toolchain install nightly-2020-10-11
 /root/.cargo/bin/rustup default nightly-2020-10-11
 
-SHELL=/bin/bash ./mach create-mach-environment
+
 SHELL=/bin/bash ./mach --no-interactive bootstrap --application-choice=browser 
 
 cp /home/resources/.mozconfig /home/mozilla-unified/.mozconfig
