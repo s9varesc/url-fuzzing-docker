893df5bd6a239f623bdbef18cb092ec69d1ec0af
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-04-05 12:21:31 +0200

    fixing chromium version

--- resources/prepchr.sh
+++ resources/prepchr.sh
@@ -3,19 +3,38 @@
 
 ##chromium
 cd /home
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git #I3233b1ddcbf50ac97a888e08f692193d4d83b3e9 (12.02)
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git 
+# checkout and update variable put depot_tools in the state necessary to build the specified chromium version 
+cd depot_tools
+git checkout 9011a5b7d57a06eb74aa39ac5db05738d811fbfb
+export DEPOT_TOOLS_UPDATE=0
 
-# 2faf61f5038edf2c408df11b9d90b601564c6745 (16.02.2021)
+# 2faf61f5038edf2c408df11b9d90b601564c6745 (16.02.2021) linux,stable,88.0.4324.182,2021-02-16
+# git fetch https://chromium.googlesource.com/chromium/src.git+refs/tags/88.0.4324.182:chromium_88.0.4324.182 --depth 1
+#git clone -b 88.0.4324.210 https://chromium.googlesource.com/chromium/src.git --depth=1
+
 mkdir /home/chromium 
 mkdir -p /home/coverageReports/chromium
 
 PATH=$PATH:/home/depot_tools
 
 cd /home/chromium
+# gclient config and git clone only neccessary for building the specified version
+git clone -b 88.0.4324.210 https://chromium.googlesource.com/chromium/src.git --depth=1
+#fetch  --no-hooks --no-history chromium     ## this fetches the current version
 
-fetch  --no-history chromium
+gclient config --spec 'solutions = [
+  {
+    "name": "src",
+    "url": "https://chromium.googlesource.com/chromium/src.git",
+    "managed": False,
+    "custom_deps": {},
+    "custom_vars": {},
+  },
+]
+'
 
-gclient sync
+gclient sync --with_branch_heads
 cd /home/chromium/src
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release sudo tzdata
@@ -23,7 +42,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release sudo tzdata
 
 ./build/install-build-deps.sh --no-prompt 
 
-gclient sync
+gclient sync --with_branch_heads
 gclient runhooks
 
 gn gen out/coverage --args="use_clang_coverage=true is_component_build=false dcheck_always_on=true is_debug=false"
