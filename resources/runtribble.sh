822cdd955fe3030e3c4ab79249e8ea8fc7ff8c7c
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-05-05 15:53:47 +0200

    updated tribble build script

--- resources/runtribble.sh
+++ resources/runtribble.sh
@@ -9,13 +9,14 @@ tribble_out_dir="$4"
 cd /home/url-fuzzing/
 git pull
 
-cp -r /home/url-fuzzing/tribble-additions/componentExtraction /home/tribble/src/main/scala/saarland/cispa/se/tribble/execution/componentExtraction
-cp /home/url-fuzzing/tribble-additions/$representations/* /home/tribble/src/main/scala/saarland/cispa/se/tribble/execution
-
+mkdir -p /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble/componentExtraction
+cp -r /home/url-fuzzing/tribble-additions/componentExtraction/* /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble/componentExtraction
+cp /home/url-fuzzing/tribble-additions/allRepresentations/* /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble
 echo "building tribble"
 cd /home/tribble
-./gradlew build >>/home/tmp/output/tribblebuild.txt
-mv ./build/libs/tribble-0.1.jar tribble.jar
+JAVA_HOME='/usr/lib/jvm/jdk-9.0.4' ./gradlew assemble  >>/home/tmp/output/tribblebuild.txt
+mv ./tribble-tool/build/libs/tribble*.jar tribble.jar
+
 
 echo "create inputs from $grammar "
 java -jar tribble.jar generate --mode=$tribble_mode --suffix=.md --grammar-file=$grammar --out-dir=$tribble_out_dir >>/home/tmp/output/tribblegen.txt
