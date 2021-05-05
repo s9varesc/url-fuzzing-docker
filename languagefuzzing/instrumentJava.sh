9cdb03e2515ead1bd6cd7e78ef5e40f5150037be
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-05-05 16:06:06 +0200

    using new java versions to build and run new tribble

--- languagefuzzing/instrumentJava.sh
+++ languagefuzzing/instrumentJava.sh
@@ -4,9 +4,8 @@ cd /home
 mkdir url-fuzzing/languagefuzzing/JavaCoverage/libs/
 cp jcov-build/jcov_30_built_jar/jcov.jar url-fuzzing/languagefuzzing/JavaCoverage/libs/
 
-apt-get install -y unzip
-cd  /usr/lib/jvm/openjdk-8
-#unzip -q src.zip
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
 
 cd /home/url-fuzzing/languagefuzzing/JavaCoverage
 java -jar libs/jcov.jar JREInstr  -implantrt libs/jcov.jar -im java.net  /usr/lib/jvm/java-8-openjdk-amd64/jre/  
