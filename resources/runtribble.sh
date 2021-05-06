afe5a049b099460d035ec6393337f65550a7f1f6
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-05-06 12:17:41 +0200

    using current tribble version

--- resources/runtribble.sh
+++ resources/runtribble.sh
@@ -14,7 +14,7 @@ cp -r /home/url-fuzzing/tribble-additions/componentExtraction/* /home/tribble/tr
 cp /home/url-fuzzing/tribble-additions/allRepresentations/* /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble
 echo "building tribble"
 cd /home/tribble
-JAVA_HOME='/usr/lib/jvm/jdk-9.0.4' ./gradlew assemble  >>/home/tmp/output/tribblebuild.txt
+./gradlew build  >>/home/tmp/output/tribblebuild.txt
 mv ./tribble-tool/build/libs/tribble*.jar tribble.jar
 
 
