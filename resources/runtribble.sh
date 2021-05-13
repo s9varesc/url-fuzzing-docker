8f90f2d2aec37f04ebaf3eceacdef067ca817aae
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-05-13 12:18:58 +0200

    improved alternate version handling

--- resources/runtribble.sh
+++ resources/runtribble.sh
@@ -12,13 +12,17 @@ git pull
 #check if there is a different tribble version available in the mounted dir /home/mountedtribble
 if [ -d "/home/mountedtribble/tribble" ]; then
   # use this version instead: allows to use experimental features
-  mv /home/mountedtribble/tribble /home/tribble
+  echo "using alternative tribble version"
+  rm -r /home/tribble
+  cp -r /home/mountedtribble/tribble /home/tribble 
 fi
 
 
 mkdir -p /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble/componentExtraction
 cp -r /home/url-fuzzing/tribble-additions/componentExtraction/* /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble/componentExtraction
-cp /home/url-fuzzing/tribble-additions/allRepresentations/* /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble
+cp /home/url-fuzzing/tribble-additions/$representations/* /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble
+cp /home/url-fuzzing/tribble-additions/Main.scala /home/tribble/tribble-tool/src/main/scala/de/cispa/se/tribble
+
 echo "building tribble"
 cd /home/tribble
 ./gradlew build  >>/home/tmp/output/tribblebuild.txt
