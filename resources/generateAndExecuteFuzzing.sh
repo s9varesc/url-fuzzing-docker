3b7c5c4a96288eb059f03d52274f91b0b74ab2e3
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-06-12 11:19:39 +0200

    fix missing arguments

--- resources/generateAndExecuteFuzzing.sh
+++ resources/generateAndExecuteFuzzing.sh
@@ -6,7 +6,7 @@ fuzz_target="$1"
 grammar="$2"
 tribble_mode="$3"
 tribble_out_dir="$4"
-eval="$5"
+
 
 # check which representations should be produced / which grammar is used; component representations only available for livingstandard-url.scala
 
@@ -41,7 +41,7 @@ cd /home/resources
 ./runtribble.sh $grammar $representations "$tribble_mode" $tribble_out_dir
 
 #./runtests.sh $fuzz_target $comp_created $tribble_out_dir 
-./executeFuzzing.sh
+./executeFuzzing.sh $fuzz_target $comp_created $tribble_out_dir
 
 
 
