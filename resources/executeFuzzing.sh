243a98fd025cf403afdc8d16cdb060559be50b32
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-06-14 11:01:54 +0200

    fix location of script execution for runtests

--- resources/executeFuzzing.sh
+++ resources/executeFuzzing.sh
@@ -6,11 +6,11 @@ comp_created="$2"
 test_dir="$3"
 
 
-
-./runtests.sh $fuzz_target $comp_created $test_dir 
+cd /home/resources
+/home/resources/runtests.sh $fuzz_target $comp_created $test_dir 
 
 cd /home/resources
-./finalizeOutput.sh
+/home/resources/finalizeOutput.sh
 
 
 
